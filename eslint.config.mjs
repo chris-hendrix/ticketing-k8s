6e1e2e2519970ef62ed8b7ee4fffa56958909e5a
Chris Hendrix <chendrix1123@gmail.com>
2024-06-09 16:18:55 -0400

    add react to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default [
   { languageOptions: { globals: globals.browser } },
@@ -15,6 +16,28 @@ export default [
         'error',
         { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_', 'caughtErrorsIgnorePattern': '^_' },
       ],
-    }
-  }
+    },
+  },
+  // Overrides for client folder with React
+  {
+    files: ['client/app/**/*.{js,jsx}'],
+    languageOptions: { globals: globals.browser },
+    ...pluginJs.configs.recommended,
+    ...pluginReactConfig,
+    rules: {
+      'quotes': ['error', 'single'], // enforce single quotes
+      'comma-dangle': 'off', // allows no comma dangle
+      'semi': ['error'], // forces semicolon
+      'no-unused-vars': [ // allows unused vars with explicit underscore
+        'error',
+        { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_', 'caughtErrorsIgnorePattern': '^_' },
+      ],
+      'react/react-in-jsx-scope': 'off', // Not needed if using React 17+
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
+  },
 ];
