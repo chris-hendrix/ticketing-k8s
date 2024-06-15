d552bc2fe1f4681182042628486779ec7cfc6469
Chris Hendrix <chendrix1123@gmail.com>
2024-06-14 20:49:12 -0400

    fetch current user on server

--- client/app/auth/signup/page.jsx
+++ client/app/auth/signup/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import useRequest from '../../../hooks/useRequest';
+import useRequest from '../../../hooks/use-request';
 
 export default () => {
   const router = useRouter();
--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,7 +1,31 @@
-export default () => {
+import axios from 'axios';
+
+const fetchCurrentUser = async () => {
+  if (typeof window === 'undefined') {
+    // server side request
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+      headers: {
+        Host: 'ticketing.dev'
+      }
+    });
+    return data;
+  } else {
+    // client side request
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+
+};
+
+const LandingPage = async () => {
+  const currentUser = await fetchCurrentUser();
+  console.log({ currentUser });
   return (
     <div>
       <h1>Landing2</h1>
     </div>
   );
 };
+
+export default LandingPage;
