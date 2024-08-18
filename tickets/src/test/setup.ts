597e72a829059aa76707044aaf13d45cbc49a8d9
Chris Hendrix <chendrix@emissary.io>
2024-08-18 15:03:01 -0400

    finish ticket routes 288

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -32,7 +32,10 @@ afterAll(async () => {
 
 
 global.signinMock = () => {
-  const payload = { email: 'test@test.com', id: '12341234' };
+  const payload = {
+    email: 'test@test.com',
+    id: new mongoose.Types.ObjectId().toHexString()
+  };
   const token = jwt.sign(payload, process.env.JWT_KEY!);
   const session = { jwt: token };
   const sessionJSON = JSON.stringify(session);
