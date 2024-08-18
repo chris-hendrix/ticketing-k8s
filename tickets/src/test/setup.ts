25c36c1957980fcbb42c73eaadc92fb56f615d71
Chris Hendrix <chendrix1123@gmail.com>
2024-08-18 15:04:43 -0400

    Create tickets service (module 13) (#8)
    * initial tickets setup
    
    * skaffod config
    
    * setup first route and tests
    
    * add ticket model and finish tests
    
    * add show route and test 282
    
    * finish ticket routes 288

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,44 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  function signinMock(): string[];
+}
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'testjwtkey';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongo.stop();
+});
+
+
+
+global.signinMock = () => {
+  const payload = {
+    email: 'test@test.com',
+    id: new mongoose.Types.ObjectId().toHexString()
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+  return [`session=${base64}`];
+};
