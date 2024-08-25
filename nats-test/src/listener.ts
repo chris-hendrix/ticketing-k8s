424dcadd09f357055928b4bb2da7d77d19fad6e9
Chris Hendrix <chendrix@emissary.io>
2024-08-25 17:46:07 -0400

    graceful shutdown 304

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,16 +1,36 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
-  subscription.on('message', (_msg) => {
-    console.log('Message received');
+  const options = stan.subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('orders-service');
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -5,12 +5,20 @@ console.clear();
 // port forward to nats-depl pod
 // kubectl get pods
 // kubectl port-forward nats-depl-7fc96b95d4-sjvqn 4222:4222
+
+// kubectl port-forward nats-depl-7fc96b95d4-sjvqn 8222:8222
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
+
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
 
   const data = JSON.stringify({
     id: '123',
