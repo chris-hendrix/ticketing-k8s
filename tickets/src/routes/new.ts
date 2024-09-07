a5140b8bba356ad4c101c0366798f39bc232f41d
Chris Hendrix <chendrix@emissary.io>
2024-09-07 16:53:41 -0400

    create nats wrapper 337

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@ticketing-k8s/common';
+import { natsWrapper } from '../nats-wrapper';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 
 const router = express.Router();
 
@@ -21,6 +23,12 @@ router.post('/api/tickets',
       userId: req.currentUser!.id
     });
     await ticket.save();
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      // userId: ticket.userId
+    });
     res.status(201).send(ticket);
   });
 
