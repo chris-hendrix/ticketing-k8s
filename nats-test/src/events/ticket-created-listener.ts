346a7f9b9f91fb8ac19855bc2166ed7e6f5c556e
Chris Hendrix <chendrix@emissary.io>
2024-09-07 13:22:58 -0400

    create listener class

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,15 @@
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Message } from 'node-nats-streaming';
+import { Listener } from './base-listener';
+
+export class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data);
+
+    msg.ack();
+  }
+}
