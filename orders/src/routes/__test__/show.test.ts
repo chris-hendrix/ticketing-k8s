7e52295c09155f77e8782e487d3dbef2ef8ebfb9
Chris <chendrix1123@gmail.coms>
2024-11-03 12:32:01 -0500

    fix orders tests 415

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,10 +1,15 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import mongoose from 'mongoose';
 
 it('fetches the order', async () => {
   // Create a ticket
-  const ticket = Ticket.build({ title: 'Concert', price: 20 });
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20,
+    id: new mongoose.Types.ObjectId().toHexString()
+  });
   await ticket.save();
 
   const user = global.signinMock();
@@ -28,7 +33,11 @@ it('fetches the order', async () => {
 
 it('returns an error if one user tries to fetch another user\'s order', async () => {
   // Create a ticket
-  const ticket = Ticket.build({ title: 'Concert', price: 20 });
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20,
+    id: new mongoose.Types.ObjectId().toHexString()
+  });
   await ticket.save();
 
   const user = global.signinMock();
